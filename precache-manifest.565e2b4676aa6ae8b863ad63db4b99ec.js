e579bd8d379b128af225de8e94f03fc5e0c41888
Touqeer Abbas <mf22-9@cs.qau.edu.pk>
2024-09-19 22:30:00 +0500

    Updates

---
+++ precache-manifest.565e2b4676aa6ae8b863ad63db4b99ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa06695e5f529d5155826c01d08b27c2",
+    "url": "%20.github.io/index.html"
+  },
+  {
+    "revision": "90e8c20e1241020e5280",
+    "url": "%20.github.io/static/css/main.d1fd6694.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "%20.github.io/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "%20.github.io/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "90e8c20e1241020e5280",
+    "url": "%20.github.io/static/js/main.34b50c82.chunk.js"
+  },
+  {
+    "revision": "aeeee05cdd8bfacf9022",
+    "url": "%20.github.io/static/js/runtime~main.5e262333.js"
+  }
+]);
